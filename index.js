20ed500020122d5f7035cdb6bab7ef5713b279f2
John <jovanpolovina@gmail.com>
2025-01-04 19:08:37 +0100

    first commit

--- index.js
+++ index.js
@@ -21,7 +21,15 @@ app.use(
   })
 );
 app.options("*", cors());
-
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://natalias-kitchen-frontend.vercel.app"
+  ); // Dodaj domen sa kog šalješ zahtev
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 // MongoDB connection
 async function main() {
   try {
