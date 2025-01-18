2bd0d6e6ca874af398f0858d135962023c96f8d2
John <jovanpolovina@gmail.com>
2025-01-18 22:19:31 +0100

    Tvoj komentar za promenu

--- src/middleware/verifyUserToken.js
+++ src/middleware/verifyUserToken.js
@@ -2,9 +2,12 @@ const admin = require("firebase-admin");
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(
-      require("../../config/firebase-adminsdk.json")
-    ),
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"), // handle newlines in private key
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    }),
+    databaseURL: process.env.FIREBASE_DATABASE_URL, // Koristite varijablu za URL baze
   });
 } else {
   admin.app(); // Ako je Firebase već inicijalizovan, koristi postojeću instancu
